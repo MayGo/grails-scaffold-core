3238b70e253ba9073e9db3a32168b90d16a11f6b
Maigo Erit <maigo.erit@smit.ee>
2015-04-27 09:57:46 +0300

    ver 1.0

--- ScaffoldCoreGrailsPlugin.groovy
+++ ScaffoldCoreGrailsPlugin.groovy
@@ -2,7 +2,7 @@ import grails.plugin.scaffold.core.ConfigUtility
 import grails.plugin.scaffold.core.DefaultTemplatesLocator
 
 class ScaffoldCoreGrailsPlugin {
-	def version = "0.5"
+	def version = "1.0"
 	def grailsVersion = "2.4 > *"
 	def loadAfter = ['bootstrap', 'hibernate', 'hibernate4']
 	def title = "Scaffold Core Plugin"
